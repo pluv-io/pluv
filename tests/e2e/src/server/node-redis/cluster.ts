5056bab513bbbdedcb96c15c4b2b4476a0f64684
leedavidcs <lee.david.cs@gmail.com>
2023-02-25 23:46:32 -0800

    chore: update hosts

--- tests/e2e/src/server/node-redis/cluster.ts
+++ tests/e2e/src/server/node-redis/cluster.ts
@@ -1,17 +1,15 @@
 import type { NatMap } from "ioredis";
 import { Cluster } from "ioredis";
 
+const host = "0.0.0.0";
 const ports = [7000, 7001, 7002, 7003, 7004, 7005];
 
-const nodes = ports.map((port) => ({
-    host: "127.0.0.1",
-    port,
-}));
+const nodes = ports.map((port) => ({ host, port }));
 
 const natMap = ports.reduce<NatMap>(
     (acc, port) => ({
         ...acc,
-        [`127.0.0.1:${port}`]: { host: "127.0.0.1", port },
+        [`${host}:${port}`]: { host: "${host}", port },
     }),
     {}
 );
