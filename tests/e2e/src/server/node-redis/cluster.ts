5c2c3c443a15a7337bbde21ac70d655b4b004cae
David Lee <lee.david.cs@gmail.com>
2023-02-26 00:25:36 -0800

    chore: updated playwright (#117)

--- tests/e2e/src/server/node-redis/cluster.ts
+++ tests/e2e/src/server/node-redis/cluster.ts
@@ -1,8 +1,17 @@
+import type { NatMap } from "ioredis";
 import { Cluster } from "ioredis";
 
-const nodes = [7000, 7001, 7002, 7003, 7004, 7005].map((port) => ({
-    host: "127.0.0.1",
-    port,
-}));
+const host = "0.0.0.0";
+const ports = [7000, 7001, 7002, 7003, 7004, 7005];
 
-export const cluster = new Cluster(nodes);
+const nodes = ports.map((port) => ({ host, port }));
+
+const natMap = ports.reduce<NatMap>(
+    (acc, port) => ({
+        ...acc,
+        [`${host}:${port}`]: { host, port },
+    }),
+    {}
+);
+
+export const cluster = new Cluster(nodes, { natMap });
