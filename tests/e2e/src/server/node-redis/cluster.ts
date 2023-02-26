e26f7ddd6938b4ba58c9a2e8ebfd8138a6a9295b
leedavidcs <lee.david.cs@gmail.com>
2023-02-25 23:41:27 -0800

    chore: add natmap

--- tests/e2e/src/server/node-redis/cluster.ts
+++ tests/e2e/src/server/node-redis/cluster.ts
@@ -1,8 +1,19 @@
+import type { NatMap } from "ioredis";
 import { Cluster } from "ioredis";
 
-const nodes = [7000, 7001, 7002, 7003, 7004, 7005].map((port) => ({
+const ports = [7000, 7001, 7002, 7003, 7004, 7005];
+
+const nodes = ports.map((port) => ({
     host: "127.0.0.1",
     port,
 }));
 
-export const cluster = new Cluster(nodes);
+const natMap = ports.reduce<NatMap>(
+    (acc, port) => ({
+        ...acc,
+        [`127.0.0.1:${port}`]: { host: "127.0.0.1", port },
+    }),
+    {}
+);
+
+export const cluster = new Cluster(nodes, { natMap });
