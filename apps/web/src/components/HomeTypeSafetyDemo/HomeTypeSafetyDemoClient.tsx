a045576a2f709ebc1388cdca5ea17707c9cc6201
David Lee <lee.david.cs@gmail.com>
2023-03-10 02:02:52 -0800

    feat: added message tooltip to client snippet (#141)

--- apps/web/src/components/HomeTypeSafetyDemo/HomeTypeSafetyDemoClient.tsx
+++ apps/web/src/components/HomeTypeSafetyDemo/HomeTypeSafetyDemoClient.tsx
@@ -1,6 +1,12 @@
 import { PrismCode } from "@pluv-internal/react-code";
 import { codeBlock } from "common-tags";
-import { CSSProperties, FC, useMemo } from "react";
+import { CSSProperties, FC, useContext, useMemo } from "react";
+import { HomeTypeSafetyDemoContext } from "./context";
+import { HomeTypeSafetyDemoClientToken } from "./HomeTypeSafetyDemoClientToken";
+
+const INPUT_PARAMETER = "__INPUT_PARAMETER__";
+const NEW_MESSAGE = "__NEW_MESSAGE__";
+const SEND_MESSAGE_INPUT = "__SEND_MESSAGE_INPUT__";
 
 export interface HomeTypeSafetyDemoClientProps {
     className?: string;
@@ -11,33 +17,59 @@ export const HomeTypeSafetyDemoClient: FC<HomeTypeSafetyDemoClientProps> = ({
     className,
     style,
 }) => {
-    const code = useMemo(
+    const template = useMemo(
         (): string => codeBlock`
+            // client/Room.tsx
+
             import { usePluvBroadcast, usePluvEvent } from "client/pluv";
             import { FC, useCallback, useState } from "react";
 
             export const Room: FC = () => {
-                const [messages, setMessages] = useState<string[]>([]);
+              const [messages, setMessages] = useState<string[]>([]);
 
-                // Listen to new messages from the server
-                usePluvEvent("MESSAGE_RECEIVED", ({ data }) => {
-                    setMessages((prev) => [...prev, data.message]);
-                });
+              // Listen to new messages from the server
+              // Get types from the SEND_MESSAGE resolver output
+              usePluvEvent("MESSAGE_RECEIVED", ({${INPUT_PARAMETER}}) => {
+                setMessages((prev) => [...prev${NEW_MESSAGE}]);
+              });
 
-                const broadcast = usePluvBroadcast();
+              const broadcast = usePluvBroadcast();
 
-                const onSubmit = useCallback((message: string) => {
-                    broadcast("SEND_MESSAGE", { message });
-                }, [broadcast]);
+              const onSubmit = useCallback((message: string) => {
+                // Broadcast to all users
+                // Get types from the SEND_MESSAGE zod input
+                broadcast("SEND_MESSAGE", {${SEND_MESSAGE_INPUT}});
+              }, [broadcast]);
 
-                // return ...
+              // return ...
             };
         `,
         []
     );
 
+    const [
+        ,
+        ,
+        ,
+        { text: inputParam },
+        { text: newMessage },
+        { text: sendMessageInput },
+    ] = useContext(HomeTypeSafetyDemoContext);
+
+    const code = useMemo(() => {
+        return template
+            .replace(INPUT_PARAMETER, inputParam)
+            .replace(NEW_MESSAGE, newMessage)
+            .replace(SEND_MESSAGE_INPUT, sendMessageInput);
+    }, [inputParam, newMessage, sendMessageInput, template]);
+
     return (
-        <PrismCode className={className} style={style} language="tsx">
+        <PrismCode
+            className={className}
+            style={style}
+            language="tsx"
+            tokenRenderer={HomeTypeSafetyDemoClientToken}
+        >
             {code}
         </PrismCode>
     );
