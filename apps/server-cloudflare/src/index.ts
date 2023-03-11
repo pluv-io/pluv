ebb287fb143e96d3bcffe370e734a738299e6bdc
leedavidcs <lee.david.cs@gmail.com>
2023-03-11 00:07:52 -0800

    refactor: clean-up cloudflare server

--- apps/server-cloudflare/src/index.ts
+++ apps/server-cloudflare/src/index.ts
@@ -1,10 +1,7 @@
 import {
     createRouter,
     DurableObjectUtils,
-    UrlUtils,
 } from "@pluv-internal/cloudflare-utils";
-import { io } from "./pluv-io";
-import { getRoomId } from "./utils";
 
 export { RoomDurableObject } from "./durable-objects";
 export { io } from "./pluv-io";
@@ -13,73 +10,23 @@ interface RouterContext {
     env: Env;
 }
 
-const router = createRouter<RouterContext>()
-    .path(
-        "get",
-        "/api/authorize",
-        async (params, { context: { env }, query }) => {
-            const roomName = (query.roomName ?? "") as string;
-            const roomId = getRoomId(roomName, env);
+const router = createRouter<RouterContext>().path(
+    "get",
+    "/api/room/:rest*",
+    async ({ rest }, { request, context: { env } }) => {
+        const [name] = rest;
 
-            if (!roomId) {
-                return new Response(
-                    JSON.stringify({
-                        error: `Room does not exist: ${roomName}`,
-                    }),
-                    {
-                        headers: { "access-control-allow-origin": "*" },
-                        status: 404,
-                    }
-                );
-            }
+        const roomId = DurableObjectUtils.isValidId(name)
+            ? env.rooms.idFromString(name)
+            : DurableObjectUtils.isValidName(name)
+            ? env.rooms.idFromName(name)
+            : env.rooms.newUniqueId();
 
-            const id = crypto.randomUUID();
+        const room = env.rooms.get(roomId);
 
-            const token = await io.createToken({
-                room: roomId.toString(),
-                user: {
-                    id,
-                    name: `user:${id}`,
-                },
-            });
-
-            return new Response(token, {
-                headers: { "access-control-allow-origin": "*" },
-                status: 200,
-            });
-        }
-    )
-    .path("post", "/api/room", async (params, { context: { env } }) => {
-        const roomId = env.rooms.newUniqueId();
-
-        return new Response(roomId.toString(), {
-            headers: { "access-control-allow-origin": "*" },
-        });
-    })
-    .path(
-        "get",
-        "/api/room/:rest*",
-        async ({ rest }, { request, context: { env } }) => {
-            const [name, ...restPath] = rest;
-            const forwardUrl = `/${restPath.join("/")}`;
-
-            const roomId = DurableObjectUtils.isValidId(name)
-                ? env.rooms.idFromString(name)
-                : DurableObjectUtils.isValidName(name)
-                ? env.rooms.idFromName(name)
-                : null;
-
-            if (!roomId) {
-                return new Response("Name is invalid", { status: 404 });
-            }
-
-            const room = env.rooms.get(roomId);
-
-            const requestUrl = UrlUtils.relative(request, forwardUrl);
-
-            return room.fetch(requestUrl, request);
-        }
-    );
+        return room.fetch(request);
+    }
+);
 
 const handler = {
     async fetch(request: Request, env: Env): Promise<Response> {
