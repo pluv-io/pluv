9eeb68a4a2fbd191b678fc2e0177fba82b7a7a7c
David Lee <lee.david.cs@gmail.com>
2023-03-14 03:21:55 -0700

    fix: fix z-indices (#162)

--- internal/react-components/src/z-indices.ts
+++ internal/react-components/src/z-indices.ts
@@ -3,5 +3,5 @@ const zIndices = ["default", "extra", "app-bar"] as const;
 export type ZIndexElement = (typeof zIndices)[number];
 
 export const getZIndex = (element: ZIndexElement): number => {
-    return zIndices.indexOf(element);
+    return zIndices.indexOf(element) * 10;
 };
