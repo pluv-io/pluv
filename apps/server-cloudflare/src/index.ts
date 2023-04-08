2cefd3ecf8b521cbb610d7dad5542d852de549f7
leedavidcs <lee.david.cs@gmail.com>
2023-04-08 16:59:10 -0700

    feat: added server cors
    feat: added server cors

--- apps/server-cloudflare/src/index.ts
+++ apps/server-cloudflare/src/index.ts
@@ -10,10 +10,40 @@ interface RouterContext {
     env: Env;
 }
 
+const getCorsHeaders = (origin: string, env: Env): Record<string, string> => {
+    if (env.DEPLOY_ENV !== "production") return {};
+
+    const corsHeaders = {
+        "Access-Control-Allow-Origin": "https://pluv.io",
+        "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+        "Access-Control-Max-Age": "86400",
+    };
+
+    const whitelist = ["pluv.io", "pluv.vercel.app"].reduce(
+        (origins, domain) => [
+            ...origins,
+            `https://${domain}`,
+            `https://www.${domain}`,
+            `wss://${domain}`,
+            `wss://www.${domain}`,
+            `ws://${domain}`,
+            `ws://www.${domain}`,
+        ],
+        [] as readonly string[]
+    );
+
+    if (!whitelist.includes(origin)) return corsHeaders;
+
+    return {
+        ...corsHeaders,
+        "Access-Control-Allow-Origin": origin,
+    };
+};
+
 const router = createRouter<RouterContext>().path(
     "get",
     "/api/room/:rest*",
-    async ({ rest }, { request, context: { env } }) => {
+    async ({ rest }, { origin, request, context: { env } }) => {
         const [name] = rest;
 
         const roomId = DurableObjectUtils.isValidId(name)
@@ -24,7 +54,11 @@ const router = createRouter<RouterContext>().path(
 
         const room = env.rooms.get(roomId);
 
-        return room.fetch(request);
+        return room.fetch(request, {
+            headers: {
+                ...getCorsHeaders(origin, env),
+            },
+        });
     }
 );
 
--- apps/server-cloudflare/src/typings.d.ts
+++ apps/server-cloudflare/src/typings.d.ts
@@ -1,3 +1,4 @@
 declare interface Env {
+    DEPLOY_ENV: "development" | "production";
     rooms: DurableObjectNamespace;
 }
