f608f1abc7b6281e6865c6675a3fcd73987507ab
leedavidcs <lee.david.cs@gmail.com>
2023-05-23 00:07:57 -0700

    chore: remove unused dependencies

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -2,11 +2,11 @@ const config = require("@pluv-internal/tailwind-config");
 
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-	...config,
-	content: [
-		"./src/components/**/*.{js,jsx,ts,tsx}",
-		"./src/pages/**/*.{js,jsx,ts,tsx}",
-		"../../internal/react-code/src/**/*.{js,jsx,ts,tsx}",
-		"../../internal/react-components/src/**/*.{js,jsx,ts,tsx}"
-	],
+    ...config,
+    content: [
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+        "./src/pages/**/*.{js,jsx,ts,tsx}",
+        "../../internal/react-code/src/**/*.{js,jsx,ts,tsx}",
+        "../../internal/react-components/src/**/*.{js,jsx,ts,tsx}",
+    ],
 };
