1122e057a10e3351be70b54ae3febe1de9f722ac
leedavidcs <lee.david.cs@gmail.com>
2023-07-14 00:52:57 -0700

    chore: delete old cli

--- packages/cli/src/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-export { createProject } from "./createProject";
-export { initializeGit } from "./initializeGit";
-export { installDependencies } from "./installDependencies";
-export { logNextSteps } from "./logNextSteps";
-export { scaffoldProject } from "./scaffoldProject";
--- packages/cli/src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export type { PackageManager } from "./getUserPkgManager";
-export { getUserPkgManager } from "./getUserPkgManager";
-export { logger } from "./logger";
-export { parseNameAndPath } from "./parseNameAndPath";
-export { renderTitle } from "./renderTitle";
-export { validateAppName } from "./validateAppName";
