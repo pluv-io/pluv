9c6dae3ab14360dadb89d71ab7e3dd22e3fcddac
leedavidcs <lee.david.cs@gmail.com>
2024-05-14 23:50:05 -0700

    chore: added builder types

--- packages/io/src/PluvProcedure.ts
+++ packages/io/src/PluvProcedure.ts
@@ -7,7 +7,7 @@ export interface PluvProcedureConfig<
     TAuthorize extends IOAuthorize<any, any, InferPlatformRoomContextType<TPlatform>>,
     TContext extends JsonObject,
     TInput extends JsonObject = {},
-    TResultBroadcast extends EventRecord<string, any> = {},
+    TOutput extends EventRecord<string, any> = {},
 > {
     input?: InputZodLike<TInput>;
     resolver?: EventResolver<
@@ -15,8 +15,9 @@ export interface PluvProcedureConfig<
         TAuthorize,
         TContext & InferPlatformRoomContextType<TPlatform> & InferPlatformEventContextType<TPlatform>,
         TInput,
-        TResultBroadcast
+        TOutput
     >;
+    type?: "broadcast" | "self" | "sync";
 }
 
 export class PluvProcedure<
@@ -24,7 +25,8 @@ export class PluvProcedure<
     TAuthorize extends IOAuthorize<any, any, InferPlatformRoomContextType<TPlatform>>,
     TContext extends JsonObject,
     TInput extends JsonObject = {},
-    TResultBroadcast extends EventRecord<string, any> = {},
+    TOutput extends EventRecord<string, any> = {},
+    TFilled extends "input" | "broadcast" | "self" | "sync" | "" = "",
 > {
     private _input: InputZodLike<TInput> | null = null;
     private _resolver: EventResolver<
@@ -32,10 +34,11 @@ export class PluvProcedure<
         TAuthorize,
         TContext & InferPlatformRoomContextType<TPlatform> & InferPlatformEventContextType<TPlatform>,
         TInput,
-        TResultBroadcast
+        TOutput
     > | null = null;
+    private _type: "broadcast" | "self" | "sync" | null = null;
 
-    public get config(): EventConfig<TPlatform, TAuthorize, TContext, TInput, TResultBroadcast, {}, {}> | null {
+    public get config(): EventConfig<TPlatform, TAuthorize, TContext, TInput, TOutput, {}, {}> | null {
         if (!this._input || !this._resolver) return null;
 
         return {
@@ -44,11 +47,12 @@ export class PluvProcedure<
         };
     }
 
-    constructor(config: PluvProcedureConfig<TPlatform, TAuthorize, TContext, TInput, TResultBroadcast> = {}) {
-        const { input, resolver } = config;
+    constructor(config: PluvProcedureConfig<TPlatform, TAuthorize, TContext, TInput, TOutput> = {}) {
+        const { input, resolver, type } = config;
 
         this._input = input ?? null;
         this._resolver = resolver ?? null;
+        this._type = type ?? null;
     }
 
     public broadcast<TResult extends EventRecord<string, any> = {}>(
@@ -59,16 +63,58 @@ export class PluvProcedure<
             TInput,
             TResult
         >,
-    ) {
-        return new PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult>({
+    ): Omit<
+        PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult, TFilled | "input" | "broadcast">,
+        TFilled | "input" | "broadcast"
+    > {
+        return new PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult, TFilled | "input" | "broadcast">({
             input: this._input ?? undefined,
             resolver,
+            type: "broadcast",
         });
     }
 
     public input<TData extends JsonObject>(
         input: InputZodLike<TData>,
-    ): PluvProcedure<TPlatform, TAuthorize, TContext, TData, {}> {
-        return new PluvProcedure<TPlatform, TAuthorize, TContext, TData, {}>({ input });
+    ): Omit<PluvProcedure<TPlatform, TAuthorize, TContext, TData, {}, TFilled | "input">, TFilled | "input"> {
+        return new PluvProcedure<TPlatform, TAuthorize, TContext, TData, {}, TFilled | "input">({ input });
+    }
+
+    public self<TResult extends EventRecord<string, any> = {}>(
+        resolver: EventResolver<
+            TPlatform,
+            TAuthorize,
+            TContext & InferPlatformRoomContextType<TPlatform> & InferPlatformEventContextType<TPlatform>,
+            TInput,
+            TResult
+        >,
+    ): Omit<
+        PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult, TFilled | "input" | "self">,
+        TFilled | "input" | "self"
+    > {
+        return new PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult, TFilled | "input" | "self">({
+            input: this._input ?? undefined,
+            resolver,
+            type: "self",
+        });
+    }
+
+    public sync<TResult extends EventRecord<string, any> = {}>(
+        resolver: EventResolver<
+            TPlatform,
+            TAuthorize,
+            TContext & InferPlatformRoomContextType<TPlatform> & InferPlatformEventContextType<TPlatform>,
+            TInput,
+            TResult
+        >,
+    ): Omit<
+        PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult, TFilled | "input" | "sync">,
+        TFilled | "input" | "sync"
+    > {
+        return new PluvProcedure<TPlatform, TAuthorize, TContext, TInput, TResult, TFilled | "input" | "sync">({
+            input: this._input ?? undefined,
+            resolver,
+            type: "sync",
+        });
     }
 }
