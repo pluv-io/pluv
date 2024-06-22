f3add874082ddfcf45e6fe1bbf76fdcbbfda1de9
leedavidcs <lee.david.cs@gmail.com>
2024-06-22 12:06:03 -0700

    WIP on docs/docs-v2

--- packages/react/src/internal/hooks/index.ts
+++ packages/react/src/internal/hooks/index.ts
@@ -1,3 +1,2 @@
 export { useRerender } from "./useRerender";
-export { useSyncExternalStore } from "./useSyncExternalStore";
 export { useSyncExternalStoreWithSelector } from "./useSyncExternalStoreWithSelector";
--- packages/react/src/internal/hooks/useSyncExternalStoreWithSelector.ts
+++ packages/react/src/internal/hooks/useSyncExternalStoreWithSelector.ts
@@ -1,1 +1,40 @@
-export { useSyncExternalStoreWithSelector } from "use-sync-external-store/with-selector.js";
+import { useRef, useSyncExternalStore } from "react";
+import { identity } from "../utils";
+
+export const useSyncExternalStoreWithSelector = <TSnapshot, TSelection>(
+    subscribe: (onStoreChange: () => void) => () => void,
+    getClientSnapshot: () => TSnapshot,
+    getServerSnapshot: undefined | null | (() => TSnapshot),
+    selector: (snapshot: TSnapshot) => TSelection,
+    isEqual?: (a: TSelection, b: TSelection) => boolean,
+): TSelection => {
+    const cacheRef = useRef<TSelection | null>(null);
+
+    const _getServerSnapshot = getServerSnapshot
+        ? () => {
+              const data = getServerSnapshot();
+
+              return selector(data);
+          }
+        : undefined;
+
+    const _getClientSnapshot = () => {
+        const data = getClientSnapshot();
+        const snapshot = selector(data);
+        const compare = isEqual ?? identity;
+        const cached = cacheRef.current;
+
+        const isUnchanged = cached === null ? snapshot === null : compare(cached, snapshot);
+
+        /**
+         * !HACK
+         * @description Memoize only the last snapshot so that we can only rerender on changes
+         * @date June 22, 2024
+         */
+        cacheRef.current = isUnchanged ? cached ?? snapshot : snapshot;
+
+        return cacheRef.current;
+    };
+
+    return useSyncExternalStore(subscribe, _getClientSnapshot, _getServerSnapshot);
+};
