7102660c54abc654c63d7ef4e5d037d08b43f11b
leedavidcs <lee.david.cs@gmail.com>
2024-06-29 21:15:44 -0700

    chore: attempt load wasm

--- internal/react-components/utils/getShiki.ts
+++ internal/react-components/utils/getShiki.ts
@@ -1,15 +1,16 @@
 import type { HighlighterGeneric } from "shiki/core";
-import { getHighlighterCore } from "shiki/core";
-import getWasm from "shiki/wasm";
+import { getHighlighterCore, loadWasm } from "shiki/core";
 
 export type ShikiLanguage = "bash" | "javascript" | "jsx" | "tsx" | "typescript";
 export type ShikiTheme = "catppuccin-latte" | "catppuccin-macchiato";
 
 export const getShiki = async () => {
+    // @ts-ignore
+    await loadWasm(import("shiki/onig.wasm"));
+
     const highlighter = await getHighlighterCore({
         themes: [import("shiki/themes/catppuccin-latte.mjs"), import("shiki/themes/catppuccin-macchiato.mjs")],
         langs: [import("shiki/langs/bash.mjs"), import("shiki/langs/tsx.mjs"), import("shiki/langs/typescript.mjs")],
-        loadWasm: getWasm,
     });
 
     return highlighter as HighlighterGeneric<ShikiLanguage, ShikiTheme>;
