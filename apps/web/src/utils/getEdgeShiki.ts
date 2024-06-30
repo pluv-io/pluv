772d44586e27cffaee15e773bf606dc25fef8297
leedavidcs <lee.david.cs@gmail.com>
2024-06-30 00:33:25 -0700

    chore: attempt import wasm

--- apps/web/src/utils/getEdgeShiki.ts
+++ apps/web/src/utils/getEdgeShiki.ts
@@ -1,12 +1,14 @@
 import type { HighlighterGeneric } from "shiki/core";
 import { getHighlighterCore, loadWasm } from "shiki/core";
+// @ts-ignore
+import wasmModule from "shiki/onig.wasm?module";
 
 export type ShikiLanguage = "bash" | "tsx" | "typescript";
 export type ShikiTheme = "catppuccin-latte" | "catppuccin-macchiato";
 
 export const getEdgeShiki = async () => {
     // @ts-ignore
-    await loadWasm(import("shiki/onig.wasm"));
+    await loadWasm(wasmModule);
 
     const highlighter = await getHighlighterCore({
         themes: [import("shiki/themes/catppuccin-latte.mjs"), import("shiki/themes/catppuccin-macchiato.mjs")],
