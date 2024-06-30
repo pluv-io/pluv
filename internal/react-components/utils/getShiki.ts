73fb51e8d2879e1d7ea7e7eaa96756c946256668
leedavidcs <lee.david.cs@gmail.com>
2024-06-29 22:36:45 -0700

    chore: conditional get wasm

--- internal/react-components/utils/getShiki.ts
+++ internal/react-components/utils/getShiki.ts
@@ -6,13 +6,30 @@ export type ShikiTheme = "catppuccin-latte" | "catppuccin-macchiato";
 
 export const getShiki = async () => {
     // @ts-ignore
-    const wasmModule = await import("./onig.wasm?module");
+    if (typeof globalThis.EdgeRuntime === "string") {
+        // @ts-ignore
+        const wasmModule = await import("./onig.wasm?module");
 
-    await WebAssembly.instantiate(wasmModule);
+        await WebAssembly.instantiate(wasmModule);
+
+        const highlighter = await getHighlighterCore({
+            themes: [import("shiki/themes/catppuccin-latte.mjs"), import("shiki/themes/catppuccin-macchiato.mjs")],
+            langs: [
+                import("shiki/langs/bash.mjs"),
+                import("shiki/langs/tsx.mjs"),
+                import("shiki/langs/typescript.mjs"),
+            ],
+        });
+
+        return highlighter;
+    }
+
+    const loadWasm = await import("shiki/wasm");
 
     const highlighter = await getHighlighterCore({
         themes: [import("shiki/themes/catppuccin-latte.mjs"), import("shiki/themes/catppuccin-macchiato.mjs")],
         langs: [import("shiki/langs/bash.mjs"), import("shiki/langs/tsx.mjs"), import("shiki/langs/typescript.mjs")],
+        loadWasm,
     });
 
     return highlighter as HighlighterGeneric<ShikiLanguage, ShikiTheme>;
