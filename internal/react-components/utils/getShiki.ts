9f8adb08058caa5e9e2129627a5e4a6a71c65e2a
David Lee <lee.david.cs@gmail.com>
2024-06-29 20:42:38 -0700

    fix: fix twoslash (#644)

--- internal/react-components/utils/getShiki.ts
+++ internal/react-components/utils/getShiki.ts
@@ -8,13 +8,7 @@ export type ShikiTheme = "catppuccin-latte" | "catppuccin-macchiato";
 export const getShiki = async () => {
     const highlighter = await getHighlighterCore({
         themes: [import("shiki/themes/catppuccin-latte.mjs"), import("shiki/themes/catppuccin-macchiato.mjs")],
-        langs: [
-            import("shiki/langs/bash.mjs"),
-            import("shiki/langs/javascript.mjs"),
-            import("shiki/langs/jsx.mjs"),
-            import("shiki/langs/tsx.mjs"),
-            import("shiki/langs/typescript.mjs"),
-        ],
+        langs: [import("shiki/langs/bash.mjs"), import("shiki/langs/tsx.mjs"), import("shiki/langs/typescript.mjs")],
         loadWasm: getWasm,
     });
 
