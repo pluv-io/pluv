e83dbbeea7610d9ad0643a727248d91ff714c8c9
leedavidcs <lee.david.cs@gmail.com>
2024-06-29 22:12:19 -0700

    chore: update external config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -20,7 +20,7 @@ const withMdx = createMdx({
 const config = {
     experimental: {
         externalDir: true,
-        serverComponentsExternalPackages: ["@shikijs/twoslash", "shiki", "vscode-oniguruma"],
+        serverComponentsExternalPackages: ["@shikijs/twoslash", "shiki", "shiki/wasm", "vscode-oniguruma"],
         /**
          * !HACK
          * @description This is to resolve ERR_REQUIRE_ESM outlined in this github issue comment
