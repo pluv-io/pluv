de1d48e62e2ea80ad2ec93634e1efdf8ddf77d3d
leedavidcs <lee.david.cs@gmail.com>
2024-10-30 03:44:25 -0700

    fix(react): fix event router types for client events

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,4 +2,10 @@ export { ConnectionState, createClient, register } from "@pluv/client";
 export type { CrdtType, InferCrdtJson } from "@pluv/crdt";
 export type { BaseUser, EventMessage, EventRecord, IOEventMessage } from "@pluv/types";
 export { createBundle } from "./createBundle";
-export type { CreateBundle, PluvProviderProps, PluvRoomProviderProps, SubscriptionHookOptions } from "./createBundle";
+export type {
+    CreateBundle,
+    EventProxy,
+    PluvProviderProps,
+    PluvRoomProviderProps,
+    SubscriptionHookOptions,
+} from "./createBundle";
