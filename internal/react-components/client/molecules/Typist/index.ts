af5a7e8cc5c4931f256fa807107173bf5623c999
leedavidcs <lee.david.cs@gmail.com>
2024-11-18 22:56:20 -0800

    chore: delete unused typist

--- internal/react-components/client/molecules/Typist/index.ts
+++
@@ -1,4 +0,0 @@
-export { Typist } from "./Typist";
-export type { TypistProps, TypistState } from "./Typist";
-export { TypistCursor } from "./TypistCursor";
-export type { TypistCursorProps } from "./TypistCursor";
