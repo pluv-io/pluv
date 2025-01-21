88b0f877af9200df35bbfd69ed85f18c70497fff
leedavidcs <lee.david.cs@gmail.com>
2025-01-21 09:40:17 -0800

    fix: fix shiki breaking changes

--- apps/web/src/utils/getShiki.ts
+++ apps/web/src/utils/getShiki.ts
@@ -1,11 +1,11 @@
 import type { HighlighterGeneric } from "shiki/core";
+import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 
 export type ShikiLanguage = "bash" | "text" | "tsx" | "typescript";
 export type ShikiTheme = "catppuccin-latte" | "catppuccin-macchiato";
 
 export const getShiki = async () => {
     const createHighlighterCore = await import("shiki/core").then((mod) => mod.createHighlighterCore);
-    const getWasm = await import("shiki/wasm");
 
     const highlighter = await createHighlighterCore({
         langAlias: {
@@ -17,7 +17,7 @@ export const getShiki = async () => {
         },
         themes: [import("shiki/themes/catppuccin-latte.mjs"), import("shiki/themes/catppuccin-macchiato.mjs")],
         langs: [import("shiki/langs/bash.mjs"), import("shiki/langs/tsx.mjs")],
-        loadWasm: getWasm,
+        engine: createOnigurumaEngine(() => import("shiki/wasm")),
     });
 
     return highlighter as HighlighterGeneric<ShikiLanguage, ShikiTheme>;
