96c5ccc8d9bf7709b77f233f998b4b3d05fa1831
leedavidcs <lee.david.cs@gmail.com>
2025-02-27 00:04:23 -0800

    feat(react): remove re-exports from client, types and crdt

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,3 @@
-export { ConnectionState, createClient, register } from "@pluv/client";
-export type { CrdtType, InferCrdtJson } from "@pluv/crdt";
-export type { BaseUser, EventMessage, EventRecord, IOEventMessage } from "@pluv/types";
 export { createBundle } from "./createBundle";
 export type {
     CreateBundle,
