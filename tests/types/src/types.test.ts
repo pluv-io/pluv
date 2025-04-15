53ce69e076ece35f1da1559be76e36971c179064
leedavidcs <lee.david.cs@gmail.com>
2025-04-14 22:37:21 -0700

    test: added type tests

---
+++ tests/types/src/types.test.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { infer as clientInfer, createClient } from "@pluv/client";
+import { createIO } from "@pluv/io";
+import { platformCloudflare } from "@pluv/platform-cloudflare";
+import { z } from "@zod/mini";
+import { expectTypeOf } from "expect-type";
+
+const io = createIO(
+    platformCloudflare({
+        authorize: {
+            secret: "",
+            user: z.object({
+                id: z.string(),
+            }),
+        },
+    }),
+);
+
+const router = io.router({
+    sendMessage: io.procedure
+        .input(
+            z.object({
+                message: z.string(),
+            }),
+        )
+        .broadcast(({ message }) => ({
+            receiveMessage: { message },
+        })),
+});
+
+const ioServer = io.server({ router });
+
+const types = clientInfer((i) => ({ io: i<typeof ioServer> }));
+const client = createClient({
+    authEndpoint: () => "",
+    types,
+});
+
+const room = client.createRoom("test-room");
+
+room.event.receiveMessage((params) => {
+    expectTypeOf<(typeof params)["data"]>().toEqualTypeOf<{ message: string }>();
+    expectTypeOf<(typeof params)["data"]>().toBeObject();
+    // @ts-expect-error
+    expectTypeOf<(typeof params)["data"]>().toEqualTypeOf<{}>();
+});
