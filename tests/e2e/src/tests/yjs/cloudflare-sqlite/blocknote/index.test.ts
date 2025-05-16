1acb9557b69585db60b9b395549781a6a4c354f6
leedavidcs <lee.david.cs@gmail.com>
2025-05-15 21:40:52 -0700

    tests: added blocknote e2e test

---
+++ tests/e2e/src/tests/yjs/cloudflare-sqlite/blocknote/index.test.ts
@@ -0,0 +1,41 @@
+import test, { expect } from "@playwright/test";
+import ms from "ms";
+import { openTestPage, waitMs } from "../../../../utils";
+
+const TEST_URL = "http://localhost:3100/yjs/cloudflare/blocknote";
+
+test.describe("CloudflareSQLite blocknote", () => {
+    test("blocknote", async () => {
+        const testUrl = `${TEST_URL}?room=sqlite-e2e-blocknote-1`;
+
+        const firstPage = await openTestPage(testUrl);
+        const secondPage = await openTestPage(testUrl);
+
+        await Promise.all([
+            firstPage.waitForSelector("#blocknote-editable"),
+            secondPage.waitForSelector("#blocknote-editable"),
+        ]);
+
+        await waitMs(ms("1s"));
+
+        await firstPage.locator("#blocknote-editable").fill("hello world");
+        await waitMs(ms("0.25s"));
+
+        await secondPage
+            .locator("#blocknote-editable")
+            .innerText()
+            .then((text) => expect(text.trim()).toEqual("hello world"));
+        await waitMs(ms("0.25s"));
+
+        await secondPage.locator("#blocknote-editable").clear();
+        await waitMs(ms("0.25s"));
+
+        await firstPage
+            .locator("#blocknote-editable")
+            .innerText()
+            .then((text) => expect(text.trim()).toEqual(""));
+
+        await firstPage.close();
+        await secondPage.close();
+    });
+});
