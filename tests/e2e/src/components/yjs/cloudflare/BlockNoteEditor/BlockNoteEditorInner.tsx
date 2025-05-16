ff96f9a107fd5473b47695929f2f5191c0bd3b98
i3dly <lee.david.cs@gmail.com>
2025-05-16 00:37:18 -0700

    tests: added blocknote tests (#1110)

---
+++ tests/e2e/src/components/yjs/cloudflare/BlockNoteEditor/BlockNoteEditorInner.tsx
@@ -0,0 +1,35 @@
+import { BlockNoteView } from "@blocknote/mantine";
+import { useCreateBlockNote } from "@blocknote/react";
+import { yjs } from "@pluv/crdt-yjs";
+import type { FC } from "react";
+import { useContext, useMemo } from "react";
+import { BlockNoteEditorContext } from "./context";
+import { getRandomUserProfile } from "./getRandomUserProfile";
+
+export interface BlockNoteEditorInnerProps {
+    userName?: string;
+}
+
+export const BlockNoteEditorInner: FC<BlockNoteEditorInnerProps> = ({ userName }) => {
+    const { doc, fragment, room } = useContext(BlockNoteEditorContext);
+
+    const provider = useMemo(() => yjs.provider({ doc, field: "blocknote", room }), [doc, room]);
+    const user = useMemo(
+        () => ({
+            ...getRandomUserProfile(),
+            ...(!!userName ? { name: userName } : {}),
+        }),
+        [userName],
+    );
+
+    const editor = useCreateBlockNote({
+        collaboration: {
+            provider,
+            fragment,
+            user,
+            showCursorLabels: "activity",
+        },
+    });
+
+    return <BlockNoteView id="blocknote-editable" editor={editor} />;
+};
---
+++ tests/e2e/src/tests/yjs/cloudflare-sqlite/blocknote/index.test.ts
@@ -0,0 +1,50 @@
+import test, { expect } from "@playwright/test";
+import ms from "ms";
+import { openTestPage, waitMs } from "../../../../utils";
+
+const TEST_URL = "http://localhost:3100/yjs/cloudflare/blocknote";
+
+const stripUsername = (text: string): string => {
+    return text
+        .replace(/[\u200B-\u200D\u2060\uFEFF]/g, "")
+        .replace(/\s*test\s*$/gi, "")
+        .trim();
+};
+
+test.describe("CloudflareSQLite blocknote", () => {
+    test("blocknote", async () => {
+        const testUrl = `${TEST_URL}?room=sqlite-e2e-blocknote-1&user=test`;
+
+        const firstPage = await openTestPage(testUrl);
+        const secondPage = await openTestPage(testUrl);
+
+        const selector = '#blocknote-editable [contenteditable="true"]';
+
+        await Promise.all([
+            firstPage.waitForSelector(selector),
+            secondPage.waitForSelector(selector),
+        ]);
+
+        await waitMs(ms("1s"));
+
+        await firstPage.locator(selector).fill("hello world");
+        await waitMs(ms("0.25s"));
+
+        await secondPage
+            .locator(selector)
+            .innerText()
+            .then((text) => expect(stripUsername(text)).toEqual("hello world"));
+        await waitMs(ms("0.25s"));
+
+        await secondPage.locator(selector).fill("");
+        await waitMs(ms("0.25s"));
+
+        await firstPage
+            .locator(selector)
+            .innerText()
+            .then((text) => expect(stripUsername(text)).toEqual(""));
+
+        await firstPage.close();
+        await secondPage.close();
+    });
+});
