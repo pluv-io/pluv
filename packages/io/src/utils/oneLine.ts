5972d1cf9ad64aa07ee8d01ad4ac8319fe29280e
leedavidcs <lee.david.cs@gmail.com>
2025-05-23 11:22:43 -0700

    fix: fix print formatting

--- packages/io/src/utils/oneLine.ts
+++ packages/io/src/utils/oneLine.ts
@@ -1,6 +1,6 @@
 export const oneLine = (strings: TemplateStringsArray, ...values: unknown[]): string => {
     return strings
-        .map((str) => str.replace(/\s+/g, " ").trim())
+        .map((str) => str.replace(/\s+/g, " "))
         .reduce((acc, str, i) => acc + str + (i < values.length ? values[i] : ""), "")
         .replace(/\s+/g, " ")
         .trim();
