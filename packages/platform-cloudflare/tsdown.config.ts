1c1dbda606df0e12f9c802fd216d7cc0741f8a30
leedavidcs <lee.david.cs@gmail.com>
2025-12-03 00:02:21 -0800

    chore: migrate to tsdown

---
+++ packages/platform-cloudflare/tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+    format: ["esm"],
+    dts: true,
+    outDir: "dist",
+    entry: "src/index.ts",
+    target: "esnext",
+    sourcemap: true,
+    clean: true,
+});
