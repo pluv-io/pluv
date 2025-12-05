a4232730785a6304c6f38baed5e164c19720234f
leedavidcs <lee.david.cs@gmail.com>
2025-12-05 00:29:01 -0800

    chore: update packages to be esm only

--- packages/persistence-cloudflare-transactional-storage/tsdown.config.ts
+++ packages/persistence-cloudflare-transactional-storage/tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-    format: ["esm", "cjs"],
+    format: ["esm"],
     dts: true,
     outDir: "dist",
     entry: "src/index.ts",
