ef109b379e8b4b997b37ab07aface2f0f7cb59df
leedavidcs <lee.david.cs@gmail.com>
2026-06-09 22:18:18 -0700

    chore: bump deps

--- tests/e2e/vite.server.config.ts
+++ tests/e2e/vite.server.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [tsconfigPaths({ projects: ["./src/server/tsconfig.json"] })],
+    resolve: {
+        tsconfigPaths: true,
+    },
     ssr: {
         external: ["better-sqlite3", "ws", "ioredis"],
         resolve: {
