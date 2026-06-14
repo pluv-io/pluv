52e78c4d60fa202aeac878e6fc43d9aabae77963
leedavidcs <lee.david.cs@gmail.com>
2026-06-14 12:40:11 -0700

    fix(crdt-loro): fix multi-runtime compat

--- packages/crdt-loro/tsdown.config.ts
+++ packages/crdt-loro/tsdown.config.ts
@@ -1,4 +1,5 @@
 import browserConfig from "./tsdown.browser.config.ts";
+import bundlerConfig from "./tsdown.bundler.config.ts";
 import nodeConfig from "./tsdown.node.config.ts";
 
-export default [browserConfig, nodeConfig];
+export default [browserConfig, bundlerConfig, nodeConfig];
