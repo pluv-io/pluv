53870061fbf23222a78fdf2ffb307a8cfcc2bc5f
i3dly <lee.david.cs@gmail.com>
2026-08-30 23:20:58 -0700

    chore: temporarily remove typescript eslint

--- packages/eslint-config-pluv/base.js
+++ packages/eslint-config-pluv/base.js
@@ -3,17 +3,11 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import onlyWarn from "eslint-plugin-only-warn";
 import eslintPluginPrettier from "eslint-plugin-prettier";
 import turboPlugin from "eslint-plugin-turbo";
-import tsEslint from "typescript-eslint";
 
 export const overrides = {
-  // Override specific rules
-  rules: {
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unnecessary-type-constraint": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "no-extra-boolean-cast": "off",
-  },
+    rules: {
+        "no-extra-boolean-cast": "off",
+    },
 };
 
 /**
@@ -22,22 +16,21 @@ export const overrides = {
  * @type {import("eslint").Linter.Config}
  * */
 export const config = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tsEslint.configs.recommended,
-  {
-    plugins: { turbo: turboPlugin },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
+    js.configs.recommended,
+    eslintConfigPrettier,
+    {
+        plugins: { turbo: turboPlugin },
+        rules: {
+            "turbo/no-undeclared-env-vars": "warn",
+        },
     },
-  },
-  { plugins: { onlyWarn } },
-  {
-    plugins: { prettier: eslintPluginPrettier },
-    rules: {
-      "prettier/prettier": ["warn", { printWidth: 100, tabWidth: 4 }],
+    { plugins: { onlyWarn } },
+    {
+        plugins: { prettier: eslintPluginPrettier },
+        rules: {
+            "prettier/prettier": ["warn", { printWidth: 100, tabWidth: 4 }],
+        },
     },
-  },
-  overrides,
-  { ignores: ["dist/**"] },
+    overrides,
+    { ignores: ["dist/**"] },
 ];
--- packages/io/src/version.ts
+++ packages/io/src/version.ts
@@ -1,3 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export { version as __PLUV_VERSION } from "./version.generated";
